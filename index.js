91777bc0158072a66267bb7da694cb14c7fe86b6
Ayush Prajapati <ayushpjpt10@gmail.com>
2024-11-21 22:56:08 +0545

    edit1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const Btn = document.getElementById("Btn");
-const firstName = document.getElementById("firstName");
-const lastName = document.getElementById("lastName");
-const userEmail = document.getElementById("userEmail");
-const userPass = document.getElementById("userPass");
-const terms = document.getElementById("terms");
 
-Btn.onclick = function(){
-  terms.textContent = `Signed in Successfully ${firstName.value} ${lastName.value}.`;
-  firstName.value = "";
-  lastName.value = "";
-  userEmail.value = "";
-  userPass.value = "";
-}
+// Store data
+sessionStorage.setItem('firstName', document.getElementById('firstName').value);
+sessionStorage.setItem('lastName', document.getElementById('lastName').value);
+sessionStorage.setItem('userEmail', document.getElementById('userEmail').value);
+sessionStorage.setItem('userPass', document.getElementById('userPass').value);
 
+// Retrieve data
+const firstName = sessionStorage.getItem('firstName');
+const lastName = sessionStorage.getItem('lastName');
+const userEmail = sessionStorage.getItem('userEmail');
+const userPass = sessionStorage.getItem('userPass');
+
+console.log(sessionStorage.getItem('firstName'));
+console.log(sessionStorage.getItem('lastName'));
+console.log(sessionStorage.getItem('userEmail'));
+console.log(sessionStorage.getItem('userPass'));
