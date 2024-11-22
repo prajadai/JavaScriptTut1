ffb240b9c140090b8b58b9bd48c8c3526d44b225
Ayush Prajapati <ayushpjpt10@gmail.com>
2024-11-22 09:18:05 +0545

    edit, working

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-
-// Store data
-sessionStorage.setItem('firstName', document.getElementById('firstName').value);
-sessionStorage.setItem('lastName', document.getElementById('lastName').value);
-sessionStorage.setItem('userEmail', document.getElementById('userEmail').value);
-sessionStorage.setItem('userPass', document.getElementById('userPass').value);
-
-// Retrieve data
-const firstName = sessionStorage.getItem('firstName');
-const lastName = sessionStorage.getItem('lastName');
-const userEmail = sessionStorage.getItem('userEmail');
-const userPass = sessionStorage.getItem('userPass');
-
-console.log(sessionStorage.getItem('firstName'));
-console.log(sessionStorage.getItem('lastName'));
-console.log(sessionStorage.getItem('userEmail'));
-console.log(sessionStorage.getItem('userPass'));
+document.getElementById("userForm").onsubmit = function(){
+    sessionStorage.setItem(
+        "firstName", document.getElementById("firstName").value
+    )
+    sessionStorage.setItem(
+        "lastName", document.getElementById("lastName").value
+    )
+    sessionStorage.setItem(
+        "userEmail", document.getElementById("userEmail").value
+    )
+    sessionStorage.setItem(
+        "userPass", document.getElementById("userPass").value
+    )
+};
